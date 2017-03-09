13b527169b35aa91e25d8635827612f9147d8b6c
nishaverma269 <vermanisha269@gmail.com>
2017-03-09 15:27:53 -0500

    Contractor Login

--- Source/app/contractorLogin/contractorLogin.js
+++ Source/app/contractorLogin/contractorLogin.js
@@ -2,9 +2,20 @@
 angular.module('webApp.contractorLogin', ['ngRoute', 'firebase']).config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/contractorLogin', {
         templateUrl: 'contractorLogin/contractorLogin.html'
-        , controller: 'ContractorCtrl'
+        , controller: 'contractorLoginCtrl'
     });
-}]).controller('ContractorCtrl', ['$scope', '$firebaseArray', '$location', 'CommonProp', function ($scope, $firebaseArray, $location, CommonProp) {
-    $scope.contractorLogin = function () {};
-    $scope.contractorLogout = function () {}
+}]).controller('contractorLoginCtrl', ['$scope', 'CommonProp', '$firebaseArray', '$firebaseObject', '$location', function ($scope, CommonProp, $firebaseArray, $firebaseObject, $location) {
+    $scope.loginPin = "";
+    $scope.contractorLogin = function () {
+        var ref = firebase.database().ref();
+        ref.child("Contractors").orderByChild("pin").equalTo($scope.loginPin).once("value", function (snapshot) {
+            var userData = snapshot.val();
+            if (userData) {
+                console.log("exists!")
+            }
+            else {
+                console.log("Fail");
+            }
+        });
+    };
 }]);
